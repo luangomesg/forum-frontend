3f295bfe3e23948ab2cd379bda092415bf6678e5
Luan <luanitachi@hotmail.com>
2026-01-29 19:51:01 -0300

    finish hub design

--- src/app/(protected)/hub/page.tsx
+++ src/app/(protected)/hub/page.tsx
@@ -3,6 +3,7 @@ import { User } from "@/src/types";
 import { cookies } from "next/headers";
 import Image from "next/image";
 import logo from "../../../assets/images/logo.png";
+import logoLg from "../../../assets/images/yourforum-logo.svg";
 
 export default async function Home() {
   const cookieStore = cookies();
@@ -16,11 +17,24 @@ export default async function Home() {
   return (
     <div className="hub">
       <header className="space-y-1">
-        <h1 className="text-3xl">Olá, {user.name} 🤗</h1>
-        <p className="text-xl">Bem vindo ao YourForum</p>
+        <h1 className="text-3xl md:text-6xl">Olá, {user.name} 🤗</h1>
+        <p className="text-xl md:text-2xl">Bem vindo ao YourForum</p>
       </header>
-      <Image src={logo} width={150} height={150} alt="YourForum Logo" />
-      <div className="flex flex-col space-y-3">
+      <Image
+        src={logo}
+        alt="YourForum Logo"
+        className="w-40 md:w-60 drop-shadow-lg lg:hidden"
+        priority
+      />
+
+      <Image
+        src={logoLg}
+        alt="YourForum Logo"
+        className="hidden lg:block drop-shadow-lg lg:col-start-1 lg:mx-auto lg:w-[60%]"
+        priority
+      />
+
+      <div className="flex flex-col space-y-3 md:space-y-4 lg:col-start-2 lg:mx-auto lg:mb-auto lg:-mt-10">
         <button>Perguntas</button>
         <button>Fazer uma pergunta</button>
       </div>
