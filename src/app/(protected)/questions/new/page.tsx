1c2c47ffdb9bf0a72c0e9e452ea1be109e19dc4d
Luan <luanitachi@hotmail.com>
2026-02-15 02:01:09 -0300

    att mobile design for new questions page

--- src/app/(protected)/questions/new/page.tsx
+++ src/app/(protected)/questions/new/page.tsx
@@ -12,6 +12,7 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { api } from "@/src/services/api";
 import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
@@ -63,46 +64,63 @@ export default function AnswersPage() {
   const isSubmitting = form.formState.isSubmitting;
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
-        <FormField
-          control={form.control}
-          name="title"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel> Titulo</FormLabel>
-              <FormControl>
-                <Input placeholder="Titulo" {...field} maxLength={60} />
-              </FormControl>
-              <div className="min-h-7 ">
-                <FormMessage className="md:text-[16px] lg:text-[18px]" />
-              </div>
-            </FormItem>
-          )}
-        ></FormField>
+      <Link href="/hub" className="absolute top-1 ">
+        <button className="border border-sidebar py-1 px-10 rounded-xl bg-primary text-primary-foreground font-bold cursor-pointer w-[80vw] hover:bg-primary/90">
+          Voltar ao HUB
+        </button>
+      </Link>
+      <div className="flex flex-col items-center w-full">
+        <div className="flex min-w-[85%] max-w-[85%] h-[15vh] border-t border-l border-r border-primary rounded-t-xl justify-center items-center bg-sidebar-primary/5">
+          <h1 className="font-bold text-[20px]">Criar Nova Pergunta</h1>
+        </div>
+        <form
+          className="flex flex-col min-w-[85%] max-w-[85%] bg-card text-card-foreground border border-primary rounded-b-xl py-6 px-6"
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel> Titulo</FormLabel>
+                <FormControl>
+                  <Input placeholder="Titulo" {...field} maxLength={60} />
+                </FormControl>
+                <div className="min-h-7 ">
+                  <FormMessage className="text-[11px] md:text-[16px] lg:text-[18px]" />
+                </div>
+              </FormItem>
+            )}
+          ></FormField>
 
-        <FormField
-          control={form.control}
-          name="body"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Descrição</FormLabel>
-              <FormControl>
-                <Textarea
-                  placeholder="Escreva sua pergunta..."
-                  {...field}
-                  maxLength={1000}
-                />
-              </FormControl>
-              <div className="min-h-7 ">
-                <FormMessage className="md:text-[16px] lg:text-[18px]" />
-              </div>
-            </FormItem>
-          )}
-        ></FormField>
-        <Button type="submit" disabled={isSubmitting}>
-          Criar pergunta
-        </Button>
-      </form>
+          <FormField
+            control={form.control}
+            name="body"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Descrição</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Escreva sua pergunta..."
+                    {...field}
+                    maxLength={1000}
+                  />
+                </FormControl>
+                <div className="min-h-7 ">
+                  <FormMessage className="text-[11px] md:text-[16px] lg:text-[18px]" />
+                </div>
+              </FormItem>
+            )}
+          ></FormField>
+          <Button
+            className=" cursor-pointer font-bold"
+            type="submit"
+            disabled={isSubmitting}
+          >
+            Criar pergunta
+          </Button>
+        </form>
+      </div>
     </Form>
   );
 }
