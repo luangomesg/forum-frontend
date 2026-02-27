09c608481a1b1eee77bb00e5600d56b23ed92198
Luan <luanitachi@hotmail.com>
2026-02-27 18:21:24 -0300

    add loading

---
+++ src/app/(protected)/questions/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-40">
+      <p>Servidor iniciando... Isso pode levar até 1 minuto.</p>
+    </div>
+  );
+}
